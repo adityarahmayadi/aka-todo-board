89e87492d6d20a49400cdd2d850bd423c288d218
adityarahmayadi <aditya.rahmayadi@gmail.com>
2022-10-25 09:30:39 +0700

    style: adjust card component styles

--- components/card/index.js
+++ components/card/index.js
@@ -6,14 +6,14 @@ export function Card({
 }){
   return(
     <div className="p-4 rounded border-slate-500/30 w-full bg-slate-400 text-slate-700 cursor-pointer">
-      <div className="flex justify-between mb-2">
+      <div className="flex justify-between mb-4 items-center">
         <p className="text-lg font-semibold text-slate-800">{title}</p>
-        <p>{category}</p>
+        <p className="font-bold text-xs">{category}</p>
       </div>
-      <p className="mb-2">
+      <p className="mb-4">
         {desc}
       </p>
-      <p className="mb-2 font-bold text-xs text-end">
+      <p className="font-bold text-xs text-end">
         {expiredAt}
       </p>
     </div>
