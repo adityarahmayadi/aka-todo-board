4bef5f2d9db019cf8fac5fd73c6479e5a7b84407
adityarahmayadi <aditya.rahmayadi@gmail.com>
2022-10-25 10:13:28 +0700

    feat: add dummy data and implement on homepage

---
+++ data/index.js
@@ -0,0 +1,58 @@
+export const data = [
+  {
+    id: 'TA-001',
+    title: 'Daily Task',
+    desc: 'This task is daily todo that need to be done',
+    expiredAt: 'Nov 24, 2022',
+    category: 'Story',
+    status: 'Todo' 
+  },
+  {
+    id: 'TA-002',
+    title: 'Daily Task',
+    desc: 'This task is daily todo that need to be done',
+    expiredAt: 'Nov 24, 2022',
+    category: 'Story',
+    status: 'Todo' 
+  },
+  {
+    id: 'TA-003',
+    title: 'Daily Task',
+    desc: 'This task is daily todo that need to be done',
+    expiredAt: 'Nov 24, 2022',
+    category: 'Story',
+    status: 'In Progress' 
+  },
+  {
+    id: 'TA-004',
+    title: 'Daily Task',
+    desc: 'This task is daily todo that need to be done',
+    expiredAt: 'Nov 24, 2022',
+    category: 'Story',
+    status: 'In Progress' 
+  },
+  {
+    id: 'TA-005',
+    title: 'Daily Task',
+    desc: 'This task is daily todo that need to be done',
+    expiredAt: 'Nov 24, 2022',
+    category: 'Story',
+    status: 'In Review' 
+  },
+  {
+    id: 'TA-006',
+    title: 'Daily Task',
+    desc: 'This task is daily todo that need to be done',
+    expiredAt: 'Nov 24, 2022',
+    category: 'Story',
+    status: 'Done' 
+  },
+  {
+    id: 'TA-007',
+    title: 'Daily Task',
+    desc: 'This task is daily todo that need to be done',
+    expiredAt: 'Nov 24, 2022',
+    category: 'Story',
+    status: 'Important' 
+  },
+]
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,42 @@
+import { useState } from "react"
 import { Board } from "../components/board"
 import { Card } from "../components/card"
 import { Header } from "../components/header"
+import { data } from '../data'
 
 export default function Home() {
+  const [items, setItems] = useState(data);
+
   return (
     <>
       <Header />
       <div className="container px-4 py-8 mx-auto">
         <div className="flex row gap-8">
-          <Board title="Todo">
-            <Card
-              title='Set up dragable board'
-              desc='Add dragable HTML api to board component, so we can drag and drop'
-              category='Important'
-              expiredAt='Nov 10, 2022'
-            />
-          </Board>
-          <Board title="In Progress" />
-          <Board title="In Review" />
-          <Board title="Done" />
+          {
+            ["Todo", "In Progress", "In Review", "Done"].map((status) => {
+              console.log({ items });
+              const count = items.filter(item => item.status === status).length;
+              return(
+                <Board
+                  title={status} key={`board-${status}`}
+                  count={count}
+                >
+                  { items
+                      .filter(item => item.status === status)
+                      .map(({ id, title, desc, category, expiredAt }) => (
+                        <Card
+                          key={id}
+                          title={title}
+                          desc={desc}
+                          category={category}
+                          expiredAt={expiredAt}
+                        />
+                      ))
+                  }
+                </Board>
+              )
+            })
+          }
         </div>
       </div>
     </>
