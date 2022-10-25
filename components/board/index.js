8bdd749ed15a3b40ad78f2522d4b9d3e7cd570fe
adityarahmayadi <aditya.rahmayadi@gmail.com>
2022-10-25 09:23:49 +0700

    feat: add button to add card on board component

--- components/board/index.js
+++ components/board/index.js
@@ -1,13 +1,33 @@
-export function Board({ title, count = 0, children }){
+import { useCallback } from "react"
+
+export function Board({
+  title,
+  count = 0,
+  children,
+  onAddCard
+}){
+  
+  const handleAddCard = useCallback(() => {
+    onAddCard?.()
+  }, [onAddCard])
+
   return(
     <div className="flex-1 bg-slate-800 highlight-white/5 ring-0 rounded p-4">
-      <div className="flex justify-between align-middle mb-8">
+      <div className="flex justify-between align-middle mb-2">
         <p className="text-slate-400 font-semibold size text-lg">
           {title}
         </p>
         <p className="text-slate-400 font-semibold size text-lg">
           {count}
         </p>
+      </div>
+      <div className="mb-4">
+        <button
+          className="py-2 px-4 rounded bg-sky-600 text-slate-100 font-semibold"
+          onClick={handleAddCard}
+        >
+          Add card
+        </button>
       </div>
       <div className="flex flex-col gap-4">
         { children }
